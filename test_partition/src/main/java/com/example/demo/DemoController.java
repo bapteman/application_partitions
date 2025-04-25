a5a3347a3923313621126afcc49f27fc31d23ac7
bseneclauze <bseneclauze@A0008530.ad.consultants-solutec.fr>
2025-04-25 10:18:08 +0200

    Commit ajout création de prtitions

--- test_partition/src/main/java/com/example/demo/DemoController.java
+++ test_partition/src/main/java/com/example/demo/DemoController.java
@@ -38,20 +38,42 @@ public class DemoController {
 	 
 	 return "<h1> test <h1>"
 	 		+ "<h2> test application <h2>"
-	 		+ "<button onclick=\"callJavaMethod()\">Click Me!</button>"
-	 		+ "<script> function callJavaMethod() { fetch('/buttonClick').then(response => response.text()).then(data => alert(data));}</script>";
+	 		+ "<button onclick=\"displayData()\">affiche les partitions</button>"
+	 		+ "<script> function displayData() { fetch('/display').then(response => response.text()).then(data => alert(data));}</script>"
+	 		+ "<button onclick=\"deleteData()\">supprime une partition</button>"
+	 		+ "<script> function deleteData() { fetch('/delete').then(response => response.text()).then(data => alert(data));}</script>"
+	 		+ "<button onclick=\"addData()\">ajoute une partition</button>"
+	 		+ "<script> function addData() { fetch('/insert').then(response => response.text()).then(data => alert(data));}</script>";
  }
  
  @Autowired
  private JdbcTemplate jdbcTemplate;
  
- @RequestMapping("/buttonClick")
+ @RequestMapping("/display")
  @ResponseBody
- public List<Partitions> handleButtonClick() throws Exception {
-     // Call your Java method here
-	 return Partitions.listPartitions(jdbcTemplate);
+ public List<Partitions> display() throws Exception {
+	return Partitions.listPartitions(jdbcTemplate);
  }
 
+ @RequestMapping("/delete")
+ @ResponseBody
+ public String delete() throws Exception{
+	try {
+		List<Partitions> partitions = Partitions.listPartitions(jdbcTemplate);
+		Partitions.deletePartition(partitions.get(partitions.size()-1).getId(), jdbcTemplate);
+		return "deleted last element";
+	} catch (Exception e) {
+		return "erreur lors de la deletion" + e.getMessage();
+	}
+	 
+ }
  
+ @RequestMapping("/insert")
+ @ResponseBody
+ public String insert(){
+	Partitions newPartition = new Partitions("nom aleatoire", "lien aléatoire");
+	Partitions.createPartition(newPartition, jdbcTemplate);
+	return "partition crée";
+ }
  
 }
--- test_partition/src/main/java/com/example/demo/Partitions.java
+++ test_partition/src/main/java/com/example/demo/Partitions.java
@@ -6,12 +6,19 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Partitions {
 
+	int id;
 	String name;
 	String lien;
 	
 	// Constructors
     public Partitions(int id, String name, String lien) {
-        this.name = name;
+    	this.id = id;
+    	this.name = name;
+        this.lien = lien;
+    }
+    
+    public Partitions(String name, String lien) {
+    	this.name = name;
         this.lien = lien;
     }
     
@@ -20,6 +27,14 @@ public class Partitions {
     }
 
     //getters and setters
+    public int getId() {
+    	return id;
+    }
+    
+    public void setId(int id) {
+    	this.id = id;
+    }
+    
     public String getName() {
         return name;
     }
@@ -45,10 +60,7 @@ public class Partitions {
     
     
     public static List<Partitions> listPartitions(JdbcTemplate jdbcTemplate) throws Exception {
-        String sql = "INSERT INTO partitions (name, lien) VALUES ("
-                + "'test', 'test.application')";
-         
-        //jdbcTemplate.update(sql);
+       
         
         String sql2 = "SELECT * FROM partitions";
         List<Partitions> partitions = jdbcTemplate.query(sql2, (rs, rowNum) -> 
@@ -58,12 +70,25 @@ public class Partitions {
         		rs.getString("lien")
        			)
        		);
-        partitions.forEach(partition -> System.out.println(partition));
         return partitions;
            
     }
     
+    public static void displayPartitions(JdbcTemplate jdbcTemplate) throws Exception {
+    	listPartitions(jdbcTemplate).forEach(partition -> System.out.println(partition));
+    }
+    
     public static void createPartition(Partitions partition, JdbcTemplate jdbcTemplate) {
-    	
+    	 String sql = "INSERT INTO partitions (name, lien) VALUES ('"
+                 + partition.getName() + "','" + partition.getLien()  + "')";
+          
+         jdbcTemplate.update(sql);
+    }
+    
+    public static void deletePartition(int id, JdbcTemplate jdbcTemplate) {
+    	 String sql = "DELETE FROM partitions WHERE id = " + id;
+          
+         jdbcTemplate.update(sql);
+         System.out.println("last element deleted");
     }
 }
